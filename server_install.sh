000573067fb711f9c9124ffa32f8cf79d36a9e99
nanotoly <43829220+nano-2-ly@users.noreply.github.com>
2024-11-29 10:50:43 +0900

    Update server_install.sh

--- server_install.sh
+++ server_install.sh
@@ -1,6 +1,7 @@
 #!bin/bash
 
-git mkdir matterhub
+mkdir matterhub
+cd matterhub
 git init
 git remote add origin https://github.com/nano-2-ly/whatsmatter-hub-flask-server.git
 git pull origin master
@@ -13,3 +14,4 @@ ngrok config add-authtoken 2ZX2X31qCQXCbNiMVDsv1qtG7Vz_2toFAYXpRQyt7rM2hURvg
 sudo sysctl -w net.ipv6.conf.all.disable_ipv6=0
 sudo sysctl -w net.ipv6.conf.default.disable_ipv6=0
 sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=0
+sudo cp docker-compose.yml /usr/share/hassio/homeassistant
