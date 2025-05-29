05e66393342d470a976bb86de54ec771595dd895
nanotoly <43829220+nano-2-ly@users.noreply.github.com>
2025-05-29 17:53:42 +0900

    Update server_install.sh

--- server_install.sh
+++ server_install.sh
@@ -5,7 +5,7 @@ cd matterhub
 git init
 git remote add origin https://github.com/nano-2-ly/whatsmatter-hub-flask-server.git
 git pull origin master
-sudo apt install python3-pip
+sudo apt install python3-pip -y
 pip install -r requirements.txt --break-system-packages
 sudo cp matterhub.service /etc/systemd/system
 
